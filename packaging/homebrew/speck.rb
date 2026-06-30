3cab39aab16ac474ade85b866975118b236ec418
Giuseppe <djtech.developer@gmail.com>
2026-07-01 00:22:59 +0200

    bump to v1.0.0-rc3

--- packaging/homebrew/speck.rb
+++ packaging/homebrew/speck.rb
@@ -1,27 +1,27 @@
 class Speck < Formula
   desc "Spec-driven agentic compiler, built on top of zerostack"
   homepage "https://github.com/gi-dellav/speck"
-  version "1.0.0-rc2"
+  version "1.0.0-rc3"
   license "GPL-3.0-only"
 
   depends_on "zerostack"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/gi-dellav/speck/releases/download/v1.0.0-rc2/speck-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/gi-dellav/speck/releases/download/v1.0.0-rc3/speck-x86_64-apple-darwin.tar.gz"
       sha256 "0000000000000000000000000000000000000000000000000000000000000000"
     else
-      url "https://github.com/gi-dellav/speck/releases/download/v1.0.0-rc2/speck-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/gi-dellav/speck/releases/download/v1.0.0-rc3/speck-aarch64-apple-darwin.tar.gz"
       sha256 "0000000000000000000000000000000000000000000000000000000000000000"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/gi-dellav/speck/releases/download/v1.0.0-rc2/speck-x86_64-unknown-linux-musl.tar.gz"
+      url "https://github.com/gi-dellav/speck/releases/download/v1.0.0-rc3/speck-x86_64-unknown-linux-musl.tar.gz"
       sha256 "0000000000000000000000000000000000000000000000000000000000000000"
     else
-      url "https://github.com/gi-dellav/speck/releases/download/v1.0.0-rc2/speck-aarch64-unknown-linux-musl.tar.gz"
+      url "https://github.com/gi-dellav/speck/releases/download/v1.0.0-rc3/speck-aarch64-unknown-linux-musl.tar.gz"
       sha256 "0000000000000000000000000000000000000000000000000000000000000000"
     end
   end
